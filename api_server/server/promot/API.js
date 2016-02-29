8ddc8704819092849672fc084c7d9eb1014dcd98
Traceray <917843296@qq.com>
2016-02-29 11:36:06 +0800

    初步完成重新构建

--- api_server/server/promot/API.js
+++ api_server/server/promot/API.js
@@ -4,8 +4,6 @@
 
 var apiServerUrl = require("config").get("apiServer");
 
-console.log(apiServerUrl);
-
 exports.promotInfo = apiServerUrl.promotinfo;//活动基本信息
 
 exports.promotionPageInfo = apiServerUrl.promoPagetinfo;//活动页面信息
--- utils/wechat/wechatUtils.js
+++ utils/wechat/wechatUtils.js
@@ -6,7 +6,7 @@
 /*!
  * 从微信的提交中提取XML文件
  */
-exports.getMessage = function (stream, callback) {
+getMessage = function (stream, callback) {
     load(stream, function (err, buf) {
         if (err) {
             return callback(err);
@@ -16,13 +16,14 @@ exports.getMessage = function (stream, callback) {
         xml2js.parseString(xml, {trim: true}, callback);
     });
 };
+exports.getMessage = getMessage;
 
 /**
  * 加载数据
  * @param stream
  * @param callback
  */
-exports.load = function (stream, callback) {
+var load = function (stream, callback) {
     // support content-type 'text/xml' using 'express-xml-bodyparser', which set raw xml string
     // to 'req.rawBody'(while latest body-parser no longer set req.rawBody), see
     // https://github.com/macedigital/express-xml-bodyparser/blob/master/lib/types/xml.js#L79
@@ -41,10 +42,13 @@ exports.load = function (stream, callback) {
     stream.once('error', callback);
 };
 
+exports.load = load;
+
 /*!
  * 将xml2js解析出来的对象转换成直接可访问的对象
  */
-exports.formatMessage = function (result) {
+
+var formatMessage = function (result) {
     var message = {};
     if (typeof result === 'object') {
         for (var key in result) {
@@ -68,3 +72,5 @@ exports.formatMessage = function (result) {
     }
     return message;
 };
+
+exports.formatMessage = formatMessage;
